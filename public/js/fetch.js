d60262d9f0843b7d3f834b7d87ede82dce9f633f
nabilramy <nabil.abushaban@gmail.com>
2022-02-23 01:06:12 +0200

    add the dom and file needed to use server relates #8

--- public/js/fetch.js
+++ public/js/fetch.js
@@ -5,19 +5,24 @@
         console.log(element)
     });
 */
-const fetch = (method, url, callback) => {
+function fetch (method, url, payload, cb) {
     const xhr = new XMLHttpRequest();
+    const payloadString = JSON.stringify(payload);
 
-    xhr.onreadystatechange = () => {
-        if (xhr.readyState == 4) {
-            if (xhr.status === 200) {
-                let response = JSON.parse(xhr.responseText);
-                return callback(response);
-            } else if (xhr.status === 404) {
-                window.location.href = "../html/error.html";
-            }
+    xhr.onreadystatechange = function () {
+      if (xhr.readyState === 4) {
+        if (xhr.status === 200) {
+          cb(JSON.parse(xhr.responseText));
+        } else {
+          cb(true);
         }
+      }
     };
-    xhr.open(method, url, false);
-    xhr.send();
-};
+    xhr.open(method, url);
+    xhr.setRequestHeader('Content-Type', 'application/json');
+    xhr.send(payloadString);
+  };
+
+  const get = function (url, cb) { fetch('GET', url, null, cb); }
+  const post = function (url, payload, cb) { fetch('POST', url, payload, cb); }
+  
