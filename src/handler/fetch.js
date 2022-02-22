c4c23d126d59342114126f2fd6b73cb88ed2522b
nabilramy <nabil.abushaban@gmail.com>
2022-02-23 01:07:55 +0200

    handled endpoint and add api functions relates #8

---
+++ src/handler/fetch.js
@@ -0,0 +1,21 @@
+const https = require('https');
+
+const fetchHandler = (link, response) => {
+  https
+    .get(link, (resp) => {
+      let data = '';
+
+      resp.on('data', (chunk) => {
+        data += chunk;
+      });
+
+      resp.on('end', () => {
+        response.writeHead(200, { "content-type": "application/json" });
+
+        response.end(data);
+      });
+    })
+    .on('error', () => {});
+};
+
+module.exports = fetchHandler;
