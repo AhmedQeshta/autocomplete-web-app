e121458d61367b9ca1db91d798e32bbe036df421
nabilramy <nabil.abushaban@gmail.com>
2022-02-23 16:13:33 +0200

    Change port

--- src/server.js
+++ src/server.js
@@ -1,12 +1,10 @@
 const http = require('http');
 const router = require('./router');
 
-const PORT = process.env.PORT || 3000;
+// const PORT = process.env.PORT || 3000;
 const HOST = process.env.HOST || 'localhost';
 
-console.log(process.env.PORT);
-
-http.createServer(router).listen(PORT, HOST, () => {
+http.createServer(router).listen(process.env.PORT || 3000, HOST, () => {
   // eslint-disable-next-line no-console
-  console.log(`Server listening on http://${HOST}:${PORT}`);
+  console.log(`Server listening on http://${HOST}:${process.env.PORT || 3000}`);
 });
