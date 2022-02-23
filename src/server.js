03c9ebafa9bb03feef458a2c46d82e6442029140
nabilramy <nabil.abushaban@gmail.com>
2022-02-23 16:15:22 +0200

    remove logs

--- src/server.js
+++ src/server.js
@@ -2,9 +2,6 @@ const http = require('http');
 const router = require('./router');
 
 // const PORT = process.env.PORT || 3000;
-const HOST = process.env.HOST || 'localhost';
+// const HOST = process.env.HOST || 'localhost';
 
-http.createServer(router).listen(process.env.PORT || 3000, HOST, () => {
-  // eslint-disable-next-line no-console
-  console.log(`Server listening on http://${HOST}:${process.env.PORT || 3000}`);
-});
+http.createServer(router).listen(process.env.PORT || 3000);
