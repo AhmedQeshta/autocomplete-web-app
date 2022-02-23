aaf7f237726c5586e31a96fa188ed2cf37973f1b
nabilramy <nabil.abushaban@gmail.com>
2022-02-23 14:43:32 +0200

    enhanced the server

--- src/server.js
+++ src/server.js
@@ -1,10 +1,10 @@
 const http = require('http');
 const router = require('./router');
+
 const PORT = process.env.PORT || 3000;
 const HOST = process.env.HOST || 'localhost';
 
-const server = http.createServer(router).listen(PORT, HOST, ()=> {
-    console.log(`Server listening on http://${HOST}:${PORT}`);
+http.createServer(router).listen(PORT, HOST, () => {
+  // eslint-disable-next-line no-console
+  console.log(`Server listening on http://${HOST}:${PORT}`);
 });
-
-
